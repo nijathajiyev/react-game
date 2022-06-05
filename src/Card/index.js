b55a7ffd507b782df8ddf877dcef9c44ce07a0cc
nijathajiyev <nijat.hajiyev7@gmail.com>
2022-06-05 18:42:02 +0400

    game

--- src/Card/index.js
+++ src/Card/index.js
@@ -4,7 +4,7 @@ export class Card extends Component {
   render() {
     let { img, name, power } = this.props.hero;
     return (
-        <div className="card" border="secondary" style={{width: "14rem", margin:"5px",display:"flex" }}>
+        <div className="card" border="secondary" style={{width: "15rem", margin:"5px",display:"flex" }}>
         <img src={img} height="150" />
         <div className="card-body">
           <h1>{name}</h1>
